4a00cfa116d1c700b3b6743314ff60f1e6fe29e7
alirezamah <alireza.mahzoon44@gmail.com>
2020-10-27 22:27:16 +0100

    add a method to return strings in an array which is substrings of the array elements

---
+++ src/main/java/se/almstudio/projects/leetcode/service/StringMatchingInAnArray.java
@@ -0,0 +1,19 @@
+package se.almstudio.projects.leetcode.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StringMatchingInAnArray {
+  public static List<String> stringMatching(String[] words) {
+    List<String> result = new ArrayList<>();
+
+    for (int i = 0; i <words.length; i++) {
+      for (int j = 0; j<words.length; j++) {
+        if (words[i].contains(words[j]) && i != j && !result.contains(words[j])) {
+          result.add(words[j]);
+        }
+      }
+    }
+    return result;
+  }
+}
